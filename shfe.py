5b94bc0c06508c90448e13bd61c4e2d9acf8c641
JuanLuis Rodriguez <Juan Luis Rodriguez@ICSG.local>
2026-05-28 16:00:51 +0100

    Start creating functions in shfe.py

--- shfe.py
+++ shfe.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import requests
+import threading
+from io import StringIO
 
-response = requests.get (f"https://www.shfe.com.cn/data/tradedata/future/dailydata/kx20260522.dat")
+dates = pd.date_range(start='2026-05-01', end='2026-05-28', freq='D')
+thread_local = threading.local()                  # Create a thread-local storage object to hold data that is specific to each thread.
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
 
-data = response.json()
-df = pd.DataFrame(data['o_curinstrument'])
+def get_session():                                   # Define a function to get a session object for the current thread.
+    if not hasattr(thread_local, "session"):         # If "thread_local" doesn't yet have a "session" attribute...
+        thread_local.session = requests.Session()    # then, create one and assign it to thread_local.session
+        thread_local.session.headers.update(HEADERS) # Assign headers to the session object, previously defined in HEADERS.
+    return thread_local.session
 
-df["product_group_index"] = pd.factorize(df["PRODUCTGROUPID"])[0] # Create a new column with the group index
+def fetch_prices_data(date): 
+    url = f"https://www.shfe.com.cn/data/tradedata/future/dailydata/kx{date.strftime('%Y%m%d')}.dat"
+    response = get_session().get(url)
+    print(response.status_code)
 
-df = df.sort_values(["product_group_index", "DELIVERYMONTH"], ascending=[True, True], kind='stable') # Sort by group index and delivery month
-df.drop(columns=["product_group_index"], inplace=True) # Remove the temporary group index column
-df.reset_index(drop=True, inplace=True) # Reset the index after sorting
+def fetch_stocks_data(date):
+    url = f"https://www.shfe.cn/data/tradedata/future/stockdata/weeklystock_{date.strftime('%Y%m%d')}/EN/all.html"
+    response = get_session().get(url)
+    print(response.status_code)
 
-df.insert(0,"M",df.groupby("PRODUCTGROUPID").cumcount() + 1) # Add a new column "M" with the cumulative count of rows within each group, starting from 1
-sub_totals_index = df.groupby("PRODUCTGROUPID").tail(1).index # Get the index of the last row of each group which turns to be the subtotal row
-df.loc[sub_totals_index, "M"] = 0 # Set the "M" value to 0 for the subtotal rows
-
-df.to_csv("shfe_prices.csv", index=False, encoding="utf-8-sig")
+def fetch_shfe_data(date):
+    fetch_prices_data(date)
+    fetch_stocks_data(date)
